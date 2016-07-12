719fc59952fcbb7f12b451f0ea76ece44ea691a0
Czar John Demafeliz <cjdemafeliz@gmail.com>
2016-07-12 22:06:57 +0800

    add, edit, demo

--- demo/ui/js/init.js
+++ demo/ui/js/init.js
@@ -5,5 +5,26 @@
   		dist: 100
   	});
 
+  	 $('.datepicker').pickadate({
+		    selectMonths: true, // Creates a dropdown to control month
+		    selectYears: 15 // Creates a dropdown of 15 years to control year
+		  });
+
+  	$("#input-search").keyup(function (event) {
+	    if (event.keyCode == 13) {
+			$('#submit-search').click();
+	    }
+	});
+
+	$('#submit-search').click(function(){
+		$('.search-results').toggle(400);
+	});
+
+	$('#submit-add').click(function(){
+		$('.validate').val('');
+		$('.vali').removeClass('active');
+		 Materialize.toast('Employee has been added! Yey!', 4000);
+	});
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
