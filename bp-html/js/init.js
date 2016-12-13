70e7fc3e598e6fcd1adb3d0326e4d0bd06375691
Czar John Demafeliz <cjdemafeliz@gmail.com>
2016-12-13 16:35:47 +0800

    add select demo

---
+++ bp-html/js/init.js
@@ -0,0 +1,9 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+
+    $('#user-dtr').material_select();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
