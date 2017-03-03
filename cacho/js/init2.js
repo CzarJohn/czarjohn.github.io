0b42f6e8f70d3eb83f0b16fb4d729135abaa2a59
Czar John Demafeliz <cjdemafeliz@gmail.com>
2017-03-03 13:55:48 +0800

    add edit delete suppliers

--- cacho/js/init2.js
+++ cacho/js/init2.js
@@ -1,16 +1,20 @@
 var item_count = 0;
 var projects = [];
+var suppliers = [];
 
 (function($){
   $(function(){
     load_projects();
-
-    //$('.view-project-modal').openModal();
-    //$('.add-project-modal').openModal();
+    load_suppliers();
     
     $('.add-project').click(function(){
       clear_add_project();
       $('.add-project-modal').openModal();
+    });
+
+    $('.add-supplier').click(function(){
+      clear_add_supplier();
+      $('.add-supplier-modal').openModal();
     });
 
     $('.add-project-modal-btn-add').click(function(){
@@ -23,6 +27,62 @@ var projects = [];
         add_project(project);
         clear_add_project();
       }
+    });
+
+    $('.add-supplier-modal-btn-add').click(function(){
+      if(!check_supplier_input()){
+        Materialize.toast('Some fields are blank.',5000);
+        setTimeout(function(){$('.add-supplier-modal').openModal();},300);
+      }
+      else{
+        supplier = get_add_supplier_input(); 
+        add_supplier(supplier);
+        clear_add_supplier();
+      }
+    });
+
+    $('.project-list').on('click', '.open-project', function(){
+      var pid = this.id.split(/-(.+)/)[1];
+      var project = find_project_code(pid);
+      if(project != null) {
+        set_view_project(project);
+        $('.view-project-modal').openModal();
+      }
+      else Materialize.toast('Project code '+pid+' cannot be found.',5000);
+    });
+
+    $('.supplier-list').on('click', '.open-supplier', function(){
+      var pid = this.id.split(/-(.+)/)[1];
+      var supplier = find_supplier_code(pid);
+      if(supplier != null) {
+        set_view_supplier(supplier);
+        $('.view-supplier-modal').openModal();
+      }
+      else Materialize.toast('Supplier id '+pid+' cannot be found.',5000);
+    });
+
+    $('.project-list').on('click', '.delete-project', function(){
+      var pid = this.id.split(/-(.+)/)[1]
+      var project = find_project_code(pid);
+      if(project != null) {
+        con = confirm('Are you sure you want to delete Project: '+project.name+'('+project['project-code']+')?\nThis cannot be undone.');
+        if(con){
+          delete_project(project);
+        }
+      }
+      else Materialize.toast('Project code '+pid+' cannot be found.',5000);
+    });
+
+    $('.supplier-list').on('click', '.delete-supplier', function(){
+      var pid = this.id.split(/-(.+)/)[1]
+      var supplier = find_supplier_code(pid);
+      if(supplier != null) {
+        con = confirm('Are you sure you want to delete Supplier: '+supplier.name+'?\nThis cannot be undone.');
+        if(con){
+          delete_supplier(supplier);
+        }
+      }
+      else Materialize.toast('Supplier id '+pid+' cannot be found.',5000);
     });
 
     $('.view-project-modal-btn-save').click(function(){
@@ -39,31 +99,22 @@ var projects = [];
       }
     });
 
-    $('.project-list').on('click', '.open-project', function(){
-      var pid = this.id.split(/-(.+)/)[1]
-      var project = find_project_code(pid);
-      if(project != null) {
-        set_view_project(project);
-        $('.view-project-modal').openModal();
+    $('.view-supplier-modal-btn-save').click(function(){
+      if(!check_supplier_edit()){
+        Materialize.toast('Some fields are blank.',5000);
+        setTimeout(function(){$('.view-supplier-modal').openModal();},300);
       }
-      else Materialize.toast('Project code '+pid+' cannot be found.',5000);
-    });
-
-    $('.project-list').on('click', '.delete-project', function(){
-      var pid = this.id.split(/-(.+)/)[1]
-      var project = find_project_code(pid);
-      if(project != null) {
-        con = confirm('Are you sure you want to delete Project: '+project.name+'('+project['project-code']+')?\nThis cannot be undone.');
-        if(con){
-          delete_project(project);
+      else{
+        supplier = get_edited_supplier();
+        if(find_supplier_code(supplier['supplier-code']) != null){
+          edit_supplier(supplier);
         }
+        else Materialize.toast('Supplier id '+pid+' cannot be found.',5000); 
       }
-      else Materialize.toast('Project code '+pid+' cannot be found.',5000);
     });
-
-
 
     $('.generate-project').click(generate_project);
+    $('.generate-supplier').click(generate_supplier);
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
@@ -80,12 +131,35 @@ function add_project(project){
   Materialize.toast('Successfully added project',3000);
 }
 
+function add_supplier(supplier){
+  suppliers.push(supplier);
+  localStorage.setItem('suppliers', JSON.stringify(arr_to_obj(suppliers)));
+  $('.supplier-list').append(
+    '<li id="'+supplier['supplier-code']+'" class="collection-item supplier">'+supplier['name']+'<span class="right teal-text">'+
+      '<i id="open-'+supplier['supplier-code']+'" class="material-icons clickable open-supplier">mode_edit</i> '+
+      '<i id="delete-'+supplier['supplier-code']+'" class="material-icons clickable delete-supplier">delete</i>'+
+    '</span></li>'
+  );
+  Materialize.toast('Successfully added supplier',3000);
+}
+
 function edit_project(project){
   $.each(projects, function(index, p){
       if(p['project-code'] == project['project-code']){
         projects[index] = project;
         localStorage.setItem('projects', JSON.stringify(arr_to_obj(projects)));
         Materialize.toast('Successfully edited project',3000);
+        return;
+      }
+  });
+}
+
+function edit_supplier(supplier){
+  $.each(suppliers, function(index, p){
+      if(p['supplier-code'] == supplier['supplier-code']){
+        suppliers[index] = supplier;
+        localStorage.setItem('suppliers', JSON.stringify(arr_to_obj(suppliers)));
+        Materialize.toast('Successfully edited supplier',3000);
         return;
       }
   });
@@ -100,6 +174,15 @@ function delete_project(project){
   Materialize.toast('Successfully deleted project',3000);
 }
 
+function delete_supplier(supplier){
+  suppliers = suppliers.filter(function( obj ) {
+    return obj['supplier-code'] !== supplier['supplier-code'];
+  });
+  localStorage.setItem('suppliers', JSON.stringify(arr_to_obj(suppliers)));
+  $('.supplier-list>#'+supplier['supplier-code']).remove();
+  Materialize.toast('Successfully deleted supplier',3000);
+}
+
 function load_projects(){
   if(localStorage.getItem('projects') != null){
     projects = obj_to_arr(JSON.parse(localStorage.getItem('projects')));
@@ -108,6 +191,20 @@ function load_projects(){
         '<li id="'+project['project-code']+'" class="collection-item project">'+project['name']+'<span class="right teal-text">'+
           '<i id="open-'+project['project-code']+'" class="material-icons clickable open-project">mode_edit</i> '+
           '<i id="delete-'+project['project-code']+'" class="material-icons clickable delete-project">delete</i>'+
+        '</span></li>'
+      );
+    });
+  }
+}
+
+function load_suppliers(){
+  if(localStorage.getItem('suppliers') != null){
+    suppliers = obj_to_arr(JSON.parse(localStorage.getItem('suppliers')));
+    $.each(suppliers, function(index, supplier){
+      $('.supplier-list').append(
+        '<li id="'+supplier['supplier-code']+'" class="collection-item supplier">'+supplier['name']+'<span class="right teal-text">'+
+          '<i id="open-'+supplier['supplier-code']+'" class="material-icons clickable open-supplier">mode_edit</i> '+
+          '<i id="delete-'+supplier['supplier-code']+'" class="material-icons clickable delete-supplier">delete</i>'+
         '</span></li>'
       );
     });
@@ -126,11 +223,33 @@ function check_project_input(){
   return true;
 }
 
+function check_supplier_input(){
+  var p = '#add-supplier-input-';
+  keys = ['name','vat-reg', 'address', 'mobile-number', 'contact-person', 'supplier-code'];
+  for(var i=0; i<keys.length; i+=1){
+    var input = $(p+keys[i]).val();
+    if(input.trim() == ''){
+      return false;
+    }
+  } 
+  return true;
+}
 
 function check_project_edit(){
-    console.log('here');
   var p = '.project-';
   keys = ['name','work-type', 'address', 'mobile-number', 'contact-person', 'project-code'];
+  for(var i=0; i<keys.length; i+=1){
+    var input = $(p+keys[i]).html();
+    if(input.trim() == ''){
+      return false;
+    }
+  } 
+  return true;
+}
+
+function check_supplier_edit(){
+  var p = '.supplier-';
+  keys = ['name','vat-reg', 'address', 'mobile-number', 'contact-person', 'supplier-code'];
   for(var i=0; i<keys.length; i+=1){
     var input = $(p+keys[i]).html();
     if(input.trim() == ''){
@@ -144,6 +263,15 @@ function find_project_code(code){
   for(var i=0; i<projects.length; i++){
     if(projects[i]['project-code'] == code){
       return projects[i];
+    }
+  }
+  return null;
+}
+
+function find_supplier_code(code){
+  for(var i=0; i<suppliers.length; i++){
+    if(suppliers[i]['supplier-code'] == code){
+      return suppliers[i];
     }
   }
   return null;
@@ -172,6 +300,15 @@ function pc_is_existing(project_code) {
   return false;
 }
 
+function sc_is_existing(supplier_code) {
+  for(var i = 0; i < suppliers.length; i++) {
+      if(suppliers[i].hasOwnProperty('supplier-code') && suppliers[i]['supplier-code'] === supplier_code) {
+          return true;
+      }
+  }
+  return false;
+}
+
 function generate_project(){
   var p = '#add-project-input-';
   $('.add-project-modal>.modal-content>.input-field>label').addClass('active');
@@ -188,11 +325,35 @@ function generate_project(){
   $(p+'project-code').val(pc);
 }
 
+function generate_supplier(){
+  var p = '#add-supplier-input-';
+  $('.add-supplier-modal>.modal-content>.input-field>label').addClass('active');
+  $(p+'name').val(generate_suppliername());
+  $(p+'vat-reg').val(generate_vatreg());
+  $(p+'address').val(generate_address());
+  $(p+'mobile-number').val(generate_mobile());
+  $(p+'contact-person').val(generate_name());
+  //ensure that there are no duplicate project codes
+  sc = generate_suppliercode();
+  while(sc_is_existing(sc)){ 
+    sc = generate_suppliercode();
+  }
+  $(p+'supplier-code').val(sc);
+}
+
 function set_view_project(project){
   var p = '.project-';
   keys = ['name','work-type', 'address', 'mobile-number', 'contact-person', 'project-code'];
   for(var i=0; i<keys.length; i+=1){
     $(p+keys[i]).html(project[keys[i]]);
+  } 
+}
+
+function set_view_supplier(supplier){
+  var p = '.supplier-';
+  keys = ['name','vat-reg', 'address', 'mobile-number', 'contact-person', 'supplier-code'];
+  for(var i=0; i<keys.length; i+=1){
+    $(p+keys[i]).html(supplier[keys[i]]);
   } 
 }
 
@@ -206,6 +367,16 @@ function get_add_project_input(){
   return project;
 }
 
+function get_add_supplier_input(){
+  var p = '#add-supplier-input-';
+  keys = ['name','vat-reg', 'address', 'mobile-number', 'contact-person', 'supplier-code'];
+  supplier = {};
+  for(var i=0; i<keys.length; i+=1){
+    supplier[keys[i]] = $(p+keys[i]).val();
+  }
+  return supplier;
+}
+
 function get_edited_project(){
   var p = '.project-';
   keys = ['name','work-type', 'address', 'mobile-number', 'contact-person', 'project-code'];
@@ -214,6 +385,16 @@ function get_edited_project(){
     project[keys[i]] = $(p+keys[i]).html();
   }
   return project;
+}
+
+function get_edited_supplier(){
+  var p = '.supplier-';
+  keys = ['name','vat-reg', 'address', 'mobile-number', 'contact-person', 'supplier-code'];
+  supplier = {};
+  for(var i=0; i<keys.length; i+=1){
+    supplier[keys[i]] = $(p+keys[i]).html();
+  }
+  return supplier;
 }
 
 function clear_add_project(){
@@ -225,5 +406,17 @@ function clear_add_project(){
     ','+p+'mobile-number'+
     ','+p+'contact-person'+
     ','+p+'project-code';
+  $(selector).val('');
+}
+
+
+function clear_add_supplier(){
+  var p = '#add-supplier-input-';
+  var selector = ''+
+    p+'name'+
+    ','+p+'vat-reg'+
+    ','+p+'address'+
+    ','+p+'mobile-number'+
+    ','+p+'contact-person';
   $(selector).val('');
 }
