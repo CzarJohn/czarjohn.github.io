ce470ba6e0734bb6c5ba89877dc8fc23a1f21b1f
Czar John Demafeliz <cjdemafeliz@gmail.com>
2021-03-27 00:42:18 +0800

    add cafe demo UI

--- chbc/js/init.js
+++
@@ -1,9 +0,0 @@
-(function($){
-  $(function(){
-
-    $('.button-collapse').sideNav();
-
-    $('#user-dtr').material_select();
-
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
